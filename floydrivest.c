ac77cb824818b85e43e4ddc78d960e977ef28f78
Irene Simó Muñoz <irenemunozsimo@gmail.com>
2022-11-01 19:57:03 +0100

    1st commit

---
+++ floydrivest.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+void print(int* A, int n){
+	for (int e=0; e<n; e++)
+		printf("%i ", A[e]);
+	printf("\n \n");
+}
+
+void swap(int* A, int i, int j){
+	int aux = A[i];
+	A[i]=A[j];
+	A[j]=aux;
+}
+
+int ranpar(int*A, int p, int q, int* count){
+	int pivot = A[q];
+	int i = (p-1);
+	for (int j=p; j<q; j++){
+		*count = *count+1;
+		if (A[j] <= pivot){
+			i++;
+			swap(A, i, j);
+		}
+	}
+	swap(A, i+1, q);
+	return (i+1);
+}
+
+void quicksort(int* A, int p, int q, int* count){
+	if (p<q){
+		int r= ranpar(A, p, q, count);
+		quicksort(A, p, r-1, count);
+		quicksort(A, r+1, q, count);
+	}
+}
+
+int main(int argc, char* argv[]){
+	clock_t t;
+    	t = clock();
+	int IN = atoi(argv[1]); // char to int
+	int S[IN],i;
+	srand ( time(NULL) ); // seed for random generator
+	for(i=0;i<IN;i++)
+     		S[i]=rand()%IN;
+	int n = ceil(pow(IN, 3./4));
+	// printf("Initial array S: ");
+	// print(S, IN);
+
+	int R[n];
+	int ranidx;	
+	for (int i=0; i<n; i++){
+		ranidx = rand()%IN;
+		R[i]=S[ranidx];
+	}
+	// printf("Sample chosen at random R: ");
+	// print(R, n);
+	int count=0;
+	quicksort(R,0, sizeof(R)/sizeof(R[0]), &count);
+	// printf("sorted R: ");
+	// print(R, n);
+	int id = floor(0.5*n-sqrt(IN));
+	int iu = floor(0.5*n+sqrt(IN));
+	int d= R[id];
+	int u=R[iu];
+	
+	// printf("d is: %i, u is: %i\n", d, u);
+	int *C= malloc(4*n*sizeof(int));
+	int j =0;
+	int ld=0;
+	for (int i=0; i<IN; i++){
+		if (S[i] >= d && S[i] <= u){
+			C[j]=S[i];
+			j++;
+			if (j > 4*n*sizeof(int)){
+				printf("Algorithm failed, E3. Aborting. \n");
+				abort();	
+			}
+		}
+		else if (S[i] < d)
+		       ld++;
+	}
+	
+	quicksort(C, 0, j-1, &count); 
+	int fin = floor(IN/2)-ld+1;
+	// printf("element fin is: %i", fin);
+	printf("Total elements: %i, Median: %i\n", IN, C[fin]);
+	t = clock() - t;
+    	double time_taken = ((double)t)/CLOCKS_PER_SEC;
+	FILE *fp;
+	fp = fopen("out/floydrivest.txt", "a+");
+	fprintf(fp, "%i %i %f\n", IN, count, time_taken);
+       	fclose(fp);	
+	printf("time taken: %f\n", time_taken);
+	return C[fin];
+}
---
+++ quickselect.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+void swap(int* A, int i, int j){
+	int aux = A[i];
+	A[i]=A[j];
+	A[j]=aux;
+}
+void print(int* A, int p, int q){
+	for (int idx=p; idx<q; idx++)
+		printf("%i ", A[idx]); // as pointer
+	printf("\n\n");
+}
+
+int ranpar(int* A,int p,int q, int* count){
+	int r=(p-1);
+	int randix = rand()%(q-p) + p;
+	swap(A, randix, q);
+	int pivot = A[q];
+	for (int e=p; e<q; e++){
+		*count = *count+1;
+		if (A[e] <= pivot){
+			r++;
+			swap(A, r, e);
+		}
+	}
+	swap(A, r+1, q); // pivot in its position
+	return (r+1);
+}
+
+void quickselect(int* A, int p, int q, int goal, int* count){
+	if (p<q){
+		int r =ranpar(A, p, q, count);
+		if (r > goal)
+			quickselect(A, p, r-1, goal, count);
+		else if (r < goal)
+			quickselect(A, r+1, q, goal, count);
+		else if (r ==goal)
+			printf("Found: %i\n", A[r]);
+	}
+}
+
+int main(int argc, char* argv[]){
+	clock_t t;
+    	t = clock();
+	int IN = atoi(argv[1]);
+	int A[IN],i;
+	srand ( time(NULL) ); // seed for random generator
+	for(i=0;i<IN;i++)
+     		A[i]=rand()%IN;
+	//printf("Original array: ");
+	//print(A, 0, IN);
+	int goal = atoi(argv[2]);
+	int count=0;	
+	quickselect(A, 0, sizeof(A)/sizeof(A[0])-1, goal, &count);
+	t = clock() - t;
+    	double time_taken = ((double)t)/CLOCKS_PER_SEC;
+	//printf("time taken: %f\n", time_taken);
+	FILE *fp;
+        fp = fopen ("out/quickselect.txt", "a+"); // file pointer
+	printf("Counter is: %i\n", count);					  
+        fprintf(fp, "%i %i %i %f\n", IN, goal, count, time_taken);
+        fclose (fp);
+	return 0;	
+}
